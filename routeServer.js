8f56846da42d2fb3289315ea0375709c156fb8c5
Leif Andrew Christian <mtmusicandart@gmail.com>
2022-11-01 08:35:10 -0600

    Update routeServer.js

--- routeServer.js
+++ routeServer.js
@@ -1,16 +1,15 @@
-
 // import { Parser } from "simple-text-parser";
 // const parser = new Parser();
 
-import fetch from 'node-fetch'
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import express, { text } from 'express';
+import fetch from "node-fetch";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import express, { text } from "express";
 
-import cors from 'cors';
-import bodyParser from 'body-parser'
+import cors from "cors";
+import bodyParser from "body-parser";
 
 // let cors = require('cors');
 // let bodyParser = require('body-parser');
@@ -20,195 +19,207 @@ const __dirname = dirname(__filename);
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(cors());
 
-let txt = '';
+let txt = "";
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/src/')));
+app.use(express.static(path.join(__dirname, "client/src/")));
 
 // An api endpoint that returns a short list of items
-app.get('/getFiles', (req, res) => { 
+app.get("/getFiles", (req, res) => {
+  var list = ["item1", "item2", "item3"];
+  //  res.json(list);
+  console.log("Sent list of items");
 
-    var list = ["item1", "item2", "item3"];
-    //  res.json(list);
-    console.log('Sent list of items');
-  
-fs.readdir('./client/src/filesToParse', function (err, files) {
+  fs.readdir("./client/src/filesToParse", function (err, files) {
     if (err) {
       console.log(err);
       return;
     }
     console.log(files);
 
-    console.log(typeof(files))
-
+    console.log(typeof files);
 
     let myArray = [...Object.values(files)];
 
     let filesAndTimes = [];
     console.log(myArray);
-    console.log(myArray[0])
+    console.log(myArray[0]);
 
     myArray.forEach((file, index) => {
-
-      
-    let { mtime } = fs.statSync(`./client/src/filesToParse/${myArray[index]}`,)
+      let { mtime } = fs.statSync(
+        `./client/src/filesToParse/${myArray[index]}`
+      );
 
       // console.log("Name:", file, "Created:", birthtime)
 
       let thingy = Date.parse(mtime);
-      console.log(thingy, 'thingy')
+      console.log(thingy, "thingy");
 
       let theObject = {
-
         Name: file,
 
-        Created: thingy
-
+        Created: thingy,
       };
 
-      filesAndTimes.push(theObject)
-    
-    })
+      filesAndTimes.push(theObject);
+    });
 
-    console.log(filesAndTimes)
+    console.log(filesAndTimes);
 
     res.json(filesAndTimes);
   });
 
-//
+  //
 
-    // res.send('backend')
+  // res.send('backend')
 });
 
-app.post('/getFile', (req, res, next) => {
-    console.log(req.query.fileName, 'req');
+app.post("/getFile", (req, res, next) => {
+  console.log(req.query.fileName, "req");
 
-    // res.status(201).json({
-    //   message: 'Thing created successfully!'
-    // });
+  // res.status(201).json({
+  //   message: 'Thing created successfully!'
+  // });
 
-    // res.send(' filename Recieved: ' + req.query?.fileName) // working rec. and send filename
+  // res.send(' filename Recieved: ' + req.query?.fileName) // working rec. and send filename
 
-    // let textFromFile = go(`./client/src/filesToParse/${req.query.fileName}`);
+  // let textFromFile = go(`./client/src/filesToParse/${req.query.fileName}`);
 
-    // console.log(textFromFile, 'aa');
+  // console.log(textFromFile, 'aa');
 
-let file = fs.readFileSync(path.resolve(__dirname, `./client/src/filesToParse/${req.query.fileName}`), 'utf8'); 
-// fileToConvertToText.ass
+  let file = fs.readFileSync(
+    path.resolve(__dirname, `./client/src/filesToParse/${req.query.fileName}`),
+    "utf8"
+  );
+  // fileToConvertToText.ass
 
-res.send(file.toString())
+  res.send(file.toString());
 
-// return (file.toString());
+  // return (file.toString());
+});
 
+app.post("/writeFile", (req, res, next) => {
+  // console.log(req.query.fileName, 'req');
 
-  });
+  console.log(req.body.file, " -file");
+  console.log(req.body.text, " -text");
 
-app.post('/writeFile', (req, res, next) => {
-    // console.log(req.query.fileName, 'req');
+  // let parsedText = JSON.parse(req.body.text);
 
-    console.log(req.body.file, " -file");
-    console.log(req.body.text, ' -text')
+  fs.writeFileSync(
+    path.resolve(__dirname, `./client/src/filesToParse/${req.body.file}`),
+    req.body.text,
+    { encoding: "utf8", flag: "w" }
+  );
 
-    // let parsedText = JSON.parse(req.body.text);
+  // let file = fs.readFileSync(path.resolve(__dirname, `./client/src/filesToParse/${req.query.fileName}`), 'utf8');
+  // fileToConvertToText.js
 
-    fs.writeFileSync(path.resolve(__dirname, `./client/src/filesToParse/${req.body.file}`), req.body.text, {encoding:'utf8',flag:'w'})
+  res.send(req.body);
+});
 
-// let file = fs.readFileSync(path.resolve(__dirname, `./client/src/filesToParse/${req.query.fileName}`), 'utf8'); 
-// fileToConvertToText.js
+app.post("/renameFile", (req, res, next) => {
+  console.log(req.body.newFileName, " -file");
+  console.log(req.body.file);
 
-res.send(req.body)
+  console.log("rename route");
+  res.send("rename route response");
 
-  });
-
-  app.post('/renameFile', (req,res,next) => {
-
-    console.log(req.body.newFileName, " -file");
-    console.log(req.body.file)
-
-    console.log('rename route');
-    res.send('rename route response')
-
-    if(req.body.newFileName.length>0){fs.rename( path.resolve(__dirname, `./client/src/filesToParse/${req.body.file}`), path.resolve(__dirname, `./client/src/filesToParse/${req.body.newFileName}`), (res)=>{console.log(res)})
+  if (req.body.newFileName.length > 0) {
+    fs.rename(
+      path.resolve(__dirname, `./client/src/filesToParse/${req.body.file}`),
+      path.resolve(
+        __dirname,
+        `./client/src/filesToParse/${req.body.newFileName}`
+      ),
+      (res) => {
+        console.log(res);
+      }
+    );
   }
+});
 
-   
-  })
+app.post("/deleteFile", (req, res, next) => {
+  console.log(req.body.fileToDelete, "file to Delete");
 
+  console.log("rename route");
+  res.send("rename route response");
 
-  app.post('/deleteFile', (req,res,next) => {
+  if (req.body.fileToDelete) {
+    // fs.rename( path.resolve(__dirname, `./client/src/filesToParse/${req.body.file}`), path.resolve(__dirname, `./client/src/filesToParse/${req.body.newFileName}`), (res)=>{console.log(res)})
 
-    console.log(req.body.fileToDelete, 'file to Delete')
-
-    console.log('rename route');
-    res.send('rename route response')
-
-    if(req.body.fileToDelete){
-      
-      // fs.rename( path.resolve(__dirname, `./client/src/filesToParse/${req.body.file}`), path.resolve(__dirname, `./client/src/filesToParse/${req.body.newFileName}`), (res)=>{console.log(res)})
-  
-      fs.unlink(path.resolve(__dirname, `./client/src/filesToParse/${req.body.fileToDelete}`), res => console.log(res))
-  
-    }
-
-   
-  })
-
-  app.post('/createFile', (req,res,next) => {
-
-    console.log(req.body.newCreatedFileName, " -new file name");
-    console.log(req.body.test, ' -created date')
-    // console.log(req.body.file)
-
-    console.log('create file route');
-    // res.send('create file route response')
-
-    if(req.body.newCreatedFileName.length>0){
-      
-      // fs.rename( path.resolve(__dirname, `./client/src/filesToParse/${req.body.file}`), path.resolve(__dirname, `./client/src/filesToParse/${req.body.newFileName}`), (res)=>{console.log(res)})
-
-      fs.appendFile(path.resolve(__dirname, `./client/src/filesToParse/${req.body.newCreatedFileName}`), "new File", (res)=>{console.log(res)})
-
-      res.send(req.body.newCreatedFileName)
-
+    fs.unlink(
+      path.resolve(
+        __dirname,
+        `./client/src/filesToParse/${req.body.fileToDelete}`
+      ),
+      (res) => console.log(res)
+    );
   }
+});
 
-   
-  })
+app.post("/createFile", (req, res, next) => {
+  console.log(req.body.newCreatedFileName, " -new file name");
+  console.log(req.body.test, " -created date");
+  // console.log(req.body.file)
+
+  console.log("create file route");
+  // res.send('create file route response')
+
+  if (req.body.newCreatedFileName.length > 0) {
+    // fs.rename( path.resolve(__dirname, `./client/src/filesToParse/${req.body.file}`), path.resolve(__dirname, `./client/src/filesToParse/${req.body.newFileName}`), (res)=>{console.log(res)})
+
+    fs.appendFile(
+      path.resolve(
+        __dirname,
+        `./client/src/filesToParse/${req.body.newCreatedFileName}`
+      ),
+      "new File",
+      (res) => {
+        console.log(res);
+      }
+    );
+
+    res.send(req.body.newCreatedFileName);
+  }
+});
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/src/'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/src/"));
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('App is listening on port ' + port);
-
+console.log("App is listening on port " + port);
 
 const go = async (fileToConvertToText) => {
+  // const __dirname = path.dirname(__filename);
 
-    // const __dirname = path.dirname(__filename);
+  let file = fs.readFileSync(
+    path.resolve(__dirname, `${fileToConvertToText}`),
+    "utf8"
+  );
+  // fileToConvertToText.ass
 
-    let file = fs.readFileSync(path.resolve(__dirname, `${fileToConvertToText}`), 'utf8'); 
-// fileToConvertToText.ass
+  console.log(file.toString());
 
-console.log(file.toString())
+  return file.toString();
 
-return (file.toString());
+  // const response = await fetch('http://jsonplaceholder.typicode.com/', {
+  //     method: 'GET',
+  //     mode: 'cors',
+  //     headers: { 'Content-Type': 'application/json' }
+  //   })
 
-// const response = await fetch('http://jsonplaceholder.typicode.com/', {
-//     method: 'GET',
-//     mode: 'cors',
-//     headers: { 'Content-Type': 'application/json' }
-//   })
-
-//   console.log(response, "typicode response")
-
-}
+  //   console.log(response, "typicode response")
+};
 
 // go('./test.txt')
